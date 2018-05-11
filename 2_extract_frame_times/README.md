600f3cc1d7ea54953f9665ed3f27b02826ad9c7c
Alessandro Miguel Sanchez <sanchez7@sh-ln03.stanford.edu>
2018-05-10 23:35:37 -0700

    add script to step 2

---
+++ 2_extract_frame_times/README.md
@@ -0,0 +1,13 @@
+Issues related to:
+.mov and .AVI
+basename shell command
+SLURM output 
+
+also: when parsing files, m is None and has no attribute group
+
+data folder not included because its really large (9.9G). contains all the show_frames stuff
+
+data/raw_output contains show_frames output
+data/parsed contains csvs for each video
+
+TODO generalize to samcam and alicecam
---
+++ 2_extract_frame_times/scripts/parse_show_frames_output.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# run this locally to take FFMPEG packet files and get the actual timestamps for frames
+
+import os, sys, re
+#from PyRTF import *
+
+#target = sys.argv[1]
+#targetfiles = os.listdir(target + "/raw")
+# print target
+# print targetfiles
+
+#v = "1614"
+
+checkfile = open("../data/parsed/checkfile.csv", "a")
+checkfile.write("subid,last.frame,length\n")
+
+#for f in filter(lambda a: a.find(".txt") != -1, targetfiles):
+# print f
+for filename in os.listdir("../data/raw"):
+
+   fin = open("../data/raw/" + filename, "r")
+   short_f = filename + ".csv" # TODO give new short name
+   fout = open("../data/parsed/" + short_f, "w")
+   fout.write("frame,pkt_time,best_time\n")
+
+   contents = fin.read().split("[FRAME]")
+   m = None
+   i = 1
+   for c in contents:
+       if c.find("media_type=video") != -1:
+           m = re.search(r"pkt_pts_time=\d+.\d+", c) or None
+	   n = re.search(r"best_effort_timestamp_time=\d+.\d+", c) or None
+           fout.write(str(i) + "," + m.group(0).replace("pkt_pts_time=", "") + "," + n.group(0).replace("best_effort_timestamp_time=","") + "\n")
+           i = i + 1
+
+   checkfile.write("XS-" + short_f + "," + str(i - 1) + "," + m.group(0).replace("pkt_pts_time=", "") + "\n")
+
+
