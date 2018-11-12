89327728254f2fd5d9be01a6736fa2db181880ec
Ketan Agrawal <agrawalk@stanford.edu>
2018-11-12 11:59:45 -0800

    test video scripts

--- 4_run_mtcnn/scripts/wrap_mtcnn_detect_faces.sh
+++ 4_run_mtcnn/scripts/wrap_mtcnn_detect_faces.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-module load tensorflow
-module load opencv/3.0.0
+module load py-tensorflow/1.4.0_py27
+module load py-pillow/5.1.0_py27
+module load opencv/3.3.0
 
-for vid_frames_dir in $PI_SCRATCH/samcam/rotated_frames/*; do
-	sbatch -p hns,normal -c 8 -t 2:00:00 --mail-type=FAIL --mail-user=sanchez7@stanford.edu \
-		    --wrap="python detect_faces.py $vid_frames_dir"
+for vid_frames_dir in $SCRATCH/testvid_rotated; do
+	sbatch -p gpu --gres gpu:1 -c 8 -t 2:00:00 --mail-type=FAIL --mail-user=agrawalk@stanford.edu \
+		    --wrap="python /scratch/users/agrawalk/samcam/4_run_mtcnn/mtcnn/detect_faces_simple.py $vid_frames_dir"
 	sleep 1
 done
