128b3f7214fd60f46c00564c8f503284fe144a34
Ketan Agrawal <agrawalk@stanford.edu>
2018-12-04 00:36:38 -0800

    email changed to ketan

--- 3_extract_and_rotate_frames/scripts/rotate_frames/wrap_rotate_frames.sh
+++ 3_extract_and_rotate_frames/scripts/rotate_frames/wrap_rotate_frames.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for vid in $PI_SCRATCH/samcam/frames/*; do
-	sbatch -p normal,hns -t 2:00:00 --mail-type=FAIL --mail-user=sanchez7@stanford.edu \
+	sbatch -p normal,hns -t 2:00:00 --mail-type=FAIL --mail-user=agrawalk@stanford.edu \
 		   --wrap="python rotate_frames.py $vid"
 	sleep 1
 done
