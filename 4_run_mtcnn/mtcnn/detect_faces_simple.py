c66ee341844fcfda00277c44c89a397c4d18a9fb
Ketan Agrawal <agrawalk@stanford.edu>
2018-12-04 00:38:43 -0800

    refactored for test videos, creates multiple csvs with appropriate names

--- 4_run_mtcnn/mtcnn/detect_faces_simple.py
+++ 4_run_mtcnn/mtcnn/detect_faces_simple.py
@@ -1,15 +1,21 @@
+"""
+script for detecting faces given a video frames folder
+NOTE: this script does not use multiprocessing.
+"""
+import csv
+# import multiprocessing
+import ntpath
 import os
 import re
 import sys
-import csv
-import face
-import ntpath
-import traceback
-import multiprocessing
-import cv2
 
-OUTPUT_FILE = "cpu_mtcnn_test.csv" 
-#OUTPUT_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/cpu_mtcnn_%s.csv")
+import cv2
+import face
+
+# import traceback
+
+
+# OUTPUT_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/cpu_mtcnn_%s.csv")
 
 
 def process_img(imgpath, detector):
@@ -18,7 +24,7 @@ def process_img(imgpath, detector):
     faces = detector.find_faces(img)
 
     name = ntpath.basename(os.path.dirname(imgpath))
-    #group = str(int(name.split("_")[1][:2]))
+    # group = str(int(name.split("_")[1][:2]))
     frame = re.search('image-(.*).jpg', ntpath.basename(imgpath)).group(1)
 
     rows = []
@@ -28,8 +34,8 @@ def process_img(imgpath, detector):
     else:
         for f in faces:
             arr = f.bounding_box
-            x, y, w, h = arr[0], arr[1], arr[2], arr[3]
-            rows.append([name, frame, True, x, y, w, h])
+            x, y, x2, y2 = arr[0], arr[1], arr[2], arr[3]
+            rows.append([name, frame, True, x, y, x2, y2])
 
     return rows
 
@@ -37,6 +43,10 @@ def process_img(imgpath, detector):
 if __name__ == "__main__":
     vid_folder = sys.argv[1]
     vid = ntpath.basename(vid_folder)
+    out_dir = os.path.join(os.path.expandvars("$SCRATCH"), 'headcam-algo/tests/output')
+
+    # this is the output CSV with face detections for a given video.
+    output_file = os.path.join(out_dir, '{}.csv'.format(vid[:-4]))
 
     detector = face.Detection()
 #    pool = multiprocessing.Pool()
@@ -44,11 +54,10 @@ if __name__ == "__main__":
 
     for image in os.listdir(vid_folder):
         results.append(process_img(os.path.join(vid_folder, image), detector))
-        #results.append(pool.apply_async(process_img, args=(os.path.join(vid_folder, image),)))
+        # results.append(pool.apply_async(process_img, args=(os.path.join(vid_folder, image),)))
 
 #    pool.close()
-
-    with open(OUTPUT_FILE, 'wb') as csvfile:
+    with open(output_file, 'wb') as csvfile:
         wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         # wr.writerow(['group', 'video', 'frame', 'is_face', 'x', 'y', 'w', 'h', 'angle'])
         for result in results:
--- 4_run_mtcnn/scripts/wrap_mtcnn_detect_faces.sh
+++ 4_run_mtcnn/scripts/wrap_mtcnn_detect_faces.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+module load cuda
 module load py-tensorflow/1.4.0_py27
 module load py-pillow/5.1.0_py27
 module load opencv/3.3.0
 
-for vid_frames_dir in $SCRATCH/testvid_rotated; do
+# currently configured for face-detecting on the test videos.
+for vid_frames_dir in $SCRATCH/headcam-algo/tests/output/*AVI; do
 	sbatch -p gpu --gres gpu:1 -c 8 -t 2:00:00 --mail-type=FAIL --mail-user=agrawalk@stanford.edu \
 		    --wrap="python /scratch/users/agrawalk/samcam/4_run_mtcnn/mtcnn/detect_faces_simple.py $vid_frames_dir"
 	sleep 1
