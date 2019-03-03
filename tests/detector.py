9017ab5a2aec1de86a2b304227590cd51d23d4c4
Ketan Agrawal <agrawalk@stanford.edu>
2019-03-03 00:52:20 -0800

    detector now returns a list (not np array) of bounding boxes

--- tests/detector.py
+++ tests/detector.py
@@ -1,15 +1,16 @@
 import numpy as np
 import cv2
-from mtcnn import face
+import mtcnn.face
 
 """
 The FaceDetector class exposes two public members
 
-self.name: the name of the detector
+self.name: the name of the detector used under the hood (currently allowed: "vj" or "mtcnn")
 
 self.detect_faces: a method which, given an image as numpy array, returns a numpy array of detected
 faces as bounding boxes [x,y,w,h].
 """
+
 class FaceDetector:
     def __init__(self,name):
         self.name = name
@@ -26,13 +27,13 @@ class FaceDetector:
 
     def __load_detector(self):
         """
-        Private member that loads the appropriate detector into self.DETECTOR_OBJECT.
+        Private member that loads the appropriate detector into self.__detector_object.
         """
         if self.name == 'vj':
             #TODO: save absolute paths to detectors as constants
             self.__detector_object = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         elif self.name == 'mtcnn':
-            self.__detector_object = face.Detection()
+            self.__detector_object = mtcnn.face.Detection()
         else:
             pass
 
@@ -42,7 +43,7 @@ class FaceDetector:
         """
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.__detector_object.detectMultiScale(gray, 1.3, 5)
-        return faces
+        return list(faces)
 
     def __mtcnn_detect(self, img):
         """
@@ -52,5 +53,5 @@ class FaceDetector:
         faces = [face.bounding_box for face in faces]
 
         #convert x1,y1,x2,y2 to x,y,w,h format
-        faces = [[bb[0], bb[1], bb[2] - bb[0], bb[3] - bb[1]] for bb in faces]
-        return np.array(faces)
+        faces = [np.array([bb[0], bb[1], bb[2] - bb[0], bb[3] - bb[1]]) for bb in faces]
+        return faces
