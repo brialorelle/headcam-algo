05825e3ce5c52ef0d67d77f33703f0c6c45e2712
Ketan Agrawal <agrawalk@stanford.edu>
2019-03-12 03:40:39 -0700

    added PCN as valid detector in Detector class

--- detector.py
+++ detector.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 import mtcnn.face
+import PCN.models
+import PCN.pcn
+import sys
 
 """
 The FaceDetector class exposes two public members
@@ -16,7 +19,8 @@ class FaceDetector:
         self.name = name
         self.__detector_lookup = {
             'vj' : self.__vj_detect,
-            'mtcnn' : self.__mtcnn_detect
+            'mtcnn' : self.__mtcnn_detect,
+            'pcn' : self.__pcn_detect
         }
         if self.name in self.__detector_lookup:
             self.detect_faces = self.__detector_lookup[self.name]
@@ -31,9 +35,13 @@ class FaceDetector:
         """
         if self.name == 'vj':
             #TODO: save absolute paths to detectors as constants
+            #TODO: change the path to the location of this in the library
             self.__detector_object = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         elif self.name == 'mtcnn':
             self.__detector_object = mtcnn.face.Detection()
+        elif self.name == 'pcn':
+            sys.path.insert(0, "/scratch/users/agrawalk/headcam-algo/PCN/")
+            self.__detector_object = PCN.models.load_model()
         else:
             pass
 
@@ -54,4 +62,14 @@ class FaceDetector:
 
         #convert x1,y1,x2,y2 to x,y,w,h format
         faces = [np.array([bb[0], bb[1], bb[2] - bb[0], bb[3] - bb[1]]) for bb in faces]
+        return faces
+
+    def __pcn_detect(self, img):
+        """
+        Private member for PCN face detection.
+        NOTE: This is a different format than x, y, w, h. PCN outputs angled bounding boxes.
+        """
+
+        faces = PCN.pcn.pcn_detect(img, self.__detector_object)
+        faces = [np.array([face.x, face.y, face.width, face.angle, face.score]) for face in faces]
         return faces
