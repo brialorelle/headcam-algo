ef697d38840d9691b943cc4764e092878ffeaac7
Ketan Agrawal <agrawalk@stanford.edu>
2019-06-20 11:25:31 -0700

    activate env

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-conda env create -f environment.yml
-mkdir $SCRATCH/.singularity 
-export SINGULARITY_CACHEDIR=$SCRATCH/.singularity 
+conda env create -f headcam.yml
+conda activate headcam
+
+#One can create $SINGULARITY_CACHEDIR wherever it's convenient on your system.
+mkdir $SCRATCH/.singularity
+export SINGULARITY_CACHEDIR=$SCRATCH/.singularity
 singularity pull docker://amsan7/openpose
