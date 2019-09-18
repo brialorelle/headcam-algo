118e5515d8b1b261b11dfd92897345d5f9502080
Ketan Agrawal <agrawalk@stanford.edu>
2019-09-18 12:04:10 -0700

    fixed echo statements

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Create Conda env
-echo Creating conda env...
+echo "Creating conda env..."
 conda env create -f headcam.yml
 conda activate headcam
 python -m ipykernel install --user --name headcam --display-name "Python (headcam)"
 
 #Pull Openpose Docker
 #TODO: change this to wherever is convenient, i.e. /path/to/.singularity.
-echo Pulling Openpose Docker...
+echo "Pulling Openpose Docker..."
 mkdir $SCRATCH/.singularity
 #One can create $SINGULARITY_CACHEDIR wherever it's convenient on your system.
 export SINGULARITY_CACHEDIR=$SCRATCH/.singularity
