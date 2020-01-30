7643dc1b36cdc9e615ea0fc7474a137e0d3e1b46
Bria Long <brialorelle@gmail.com>
2020-01-29 16:22:19 -0800

    reorg analysis scripts

---
+++ analysis/get_all_openpose_bounding_boxes.R
@@ -0,0 +1,43 @@
+
+# load bounding boxes from openpose detections from the entire dataset
+# (created in get_bounding_boxes_per_vid.py using numpy vectors in $GROUP_SCRATCH/openpose_flattened_whole)
+
+require(data.table)
+require(here)
+require(stringr)
+
+files <- list.files("../src/bounding_boxes") # 1632 vids
+# A_20140630_2117_04_bounding_boxes.csv had no detections
+
+load("../src/data/vid_info.RData") # vid_info
+downup = fread("../src/data/video_right-side-up.csv", stringsAsFactors = F)
+
+df <- data.frame()
+
+#had to specify columns to get rid of the total column
+for (i in 1:length(files)) {
+  tmp <- fread(paste("../src/bounding_boxes", files[i], sep='/'), stringsAsFactors = F) 
+  if(ncol(tmp)==9) {
+    tmp$V1 = NULL
+    this_vid = str_remove(files[i], "_bounding_boxes.csv")
+    tmp$child_id = strsplit(files[i], '_')[[1]][1]
+    tmp$vid_name = this_vid
+    tmp$age_days = vid_info[which(vid_info$vid_name == this_vid)[1],]$age_days
+    tmp$right_side_up = downup[which(downup$video == this_vid),]$right_side_up
+    # remove any rows with all 0 bounding box + confidences (~50%)
+    tmp = subset(tmp, mean_confidence!=0)
+    #df <- rbindlist(list(df, tmp)) # , use.names = T
+    df = rbind(df, tmp)
+  } else {
+    print(paste("check file: ",files[i]))
+  }
+}
+
+save(df, file="all_bounding_boxes.RData")
+
+
+df$area = df$height * df$width
+require(ggplot2)
+df %>% filter(area>0) %>%
+  ggplot(aes(x=area, color=label, fill=label)) + 
+  geom_histogram(alpha=.5) 
