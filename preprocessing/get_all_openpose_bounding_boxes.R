59a6b9d45d11809305186973ebecaf22dec2fe6f
kachergis <george.kachergis@gmail.com>
2020-04-20 12:13:04 -0700

    re-exported bounding boxes from numpy arrays to create all_bounding_boxes.RData - still need frame numbers for Y vids

--- preprocessing/get_all_openpose_bounding_boxes.R
+++ preprocessing/get_all_openpose_bounding_boxes.R
@@ -6,23 +6,29 @@ require(data.table)
 require(here)
 require(stringr)
 
-files <- list.files("../src/bounding_boxes") # 1632 vids
+files <- list.files(here("bounding_boxes")) # 1927 vids
 # A_20140630_2117_04_bounding_boxes.csv had no detections
 
-load("../src/data/vid_info.RData") # vid_info
-downup = fread("../src/data/video_right-side-up.csv", stringsAsFactors = F)
+load(here("src/data/vid_info.RData")) # vid_info
+downup = fread(here("src/data/video_right-side-up.csv"), stringsAsFactors = F)
 
 df <- data.frame()
 
+Yb <- as.Date("20180214", "%Y%m%d")
+
 #had to specify columns to get rid of the total column
 for (i in 1:length(files)) {
-  tmp <- fread(paste("../src/bounding_boxes", files[i], sep='/'), stringsAsFactors = F) 
+  tmp <- fread(paste(here("bounding_boxes"), files[i], sep='/'), stringsAsFactors = F) 
   if(ncol(tmp)==9) {
     tmp$V1 = NULL
     this_vid = str_remove(files[i], "_bounding_boxes.csv")
     tmp$child_id = strsplit(files[i], '_')[[1]][1]
     tmp$vid_name = this_vid
     tmp$age_days = vid_info[which(vid_info$vid_name == this_vid)[1],]$age_days
+    if(is.na(tmp$age_days) & tmp$child_id=="Y") {
+      cur_age = as.Date(strsplit(files[i], '_')[[1]][2], "%Y%m%d")
+      tmp$age_days = as.numeric(cur_age - Yb)
+    }
     tmp$right_side_up = downup[which(downup$video == this_vid),]$right_side_up
     # remove any rows with all 0 bounding box + confidences (~50%)
     tmp = subset(tmp, mean_confidence!=0)
@@ -33,11 +39,9 @@ for (i in 1:length(files)) {
   }
 }
 
-save(df, file="all_bounding_boxes.RData")
-
+df <- df %>% mutate(height = ifelse(child_id=="Y", height/480, height),
+                    width = ifelse(child_id=="Y", width/640, width))
 
 df$area = df$height * df$width
-require(ggplot2)
-df %>% filter(area>0) %>%
-  ggplot(aes(x=area, color=label, fill=label)) + 
-  geom_histogram(alpha=.5) 
+save(df, file="data/all_bounding_boxes.RData")
+
