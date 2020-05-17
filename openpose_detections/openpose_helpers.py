b9319e1c5e3385ead671fcfcb14b67920110ce50
Ketan Agrawal <agrawalk@stanford.edu>
2020-05-17 00:44:18 -0700

    added json filepaths => numpy

--- openpose_detections/openpose_helpers.py
+++ openpose_detections/openpose_helpers.py
@@ -1,19 +1,18 @@
-import ujson as json
 import pandas as pd
 import numpy as np
 from itertools import chain
 import time
 import msgpack
-import multiprocessing
-import json
+import multiprocessing as mp # hack to get mp access to process
 import os
 import ntpath
+import ujson
+
 
 from utils import submit_job
 from config import *
 
-#TODO: run_openpose() if condense() is true, the job that it submits also condenses once the openpose finishes.
-
+# TODO run_openpose() if condense() is true, the job that it submits also condenses once the openpose finishes.
 
 def run_openpose(vid_path, op_output_dir, face=True, hand=True, overwrite=False, **kwargs):
     """run_openpose: submit sbatch job to run Openpose on given video.
@@ -57,7 +56,6 @@ def run_openpose(vid_path, op_output_dir, face=True, hand=True, overwrite=False,
     msg = submit_job(cmd, job_name=f'{vid_name}', p='gpu', t=5.0, mem='8G', gres='gpu:1')
     print('command line output: ', msg)
 
-
 def collect_filepaths(frame_df, portion_size=2000000, verbose=False):
     """collect_filepaths: aggregates Openpose filepaths from given frame dataframe.
 
@@ -75,7 +73,6 @@ def collect_filepaths(frame_df, portion_size=2000000, verbose=False):
         p = multiprocessing.Pool()
         fps.extend(p.map(_openpose_filepath, data))
     return fps
-
 
 def condense_openpose(frame_df, fps=None, overwrite=False):
     """condense_openpose: condenses folders of frame-level JSON outputs into video-level JSON
@@ -97,6 +94,81 @@ def condense_openpose(frame_df, fps=None, overwrite=False):
     g = frame_df.groupby('vid_name')
     g.apply(lambda df: _save_vid_json(df, overwrite))
 
+# def jsons_to_npy(json_filepaths):
+def get_json_filepaths(vid_name, num_frames):
+    # TODO: don't hardcode stuff, use OPENPOSE_OUTPUT_DIR
+    GROUP_SCRATCH = '/scratch/groups/mcfrank/'
+    vid_dir = os.path.join(GROUP_SCRATCH,
+                        'Home_Headcam',
+                        'openpose_raw_json',
+                        vid_name)
+    json_name = lambda i: f'{vid_name}_{str(i).zfill(12)}_keypoints.json'
+    fps = [os.path.join(vid_dir, json_name(i))
+        for i in range(0, num_frames)]
+    return fps
+
+def load_json_chunk(chunk):
+    return [ujson.load(open(f, 'r')) for f in chunk]
+
+def load_json_list(json_filepaths, part_size=10000, num_chunks=16):
+    num_frames = len(json_filepaths)
+    chunks = lambda fps, chunk_size: [fps[i:i+chunk_size]
+                                    for i in range(0, part_size, chunk_size)]
+    json_list = []
+    print('loading json files...')
+    for i in range(0, num_frames, part_size):
+        print(f'{i}/{num_frames}')
+        fps_cut = json_filepaths[i:i+part_size]
+        p = mp.Pool()
+        chunk_size = len(fps_cut) // num_chunks
+        chunk_list = chunks(fps_cut, chunk_size)
+        json_list += p.map(load_json_chunk, chunk_list)
+    json_list = [frame for chunk in json_list for frame in chunk]
+    print('done loading json files.')
+    return json_list
+
+def json_list_to_npy(json_list):
+    """json_list_to_npy: converts list of json (dictionaries)
+    for each frame to one numpy array
+
+    :param json_list: list of dictionaries corresponding to Openpose keypoints
+    for each frame of the video
+
+    :return: numpy array containing Openpose keypoints for each person in each frame.
+    the shape is as follows:
+
+    (num_frames, max_num_people, (x, y, conf) = 3, num_keypoints = 130)
+
+    in other words, for each frame, for each person in that frame,
+    we have their keypoints stored in 3 slices of length 130,
+    one for each of (x, y, conf).
+
+    for frames with less than max_num_people, the remaining people's slices are filled with
+    np.NaN.
+    """
+    def frame_to_npy(frame, max_num_people):
+        """frame_to_npy: converts a single frame to npy
+        """
+
+        arr = np.full((max_num_people, 3, OPENPOSE_NUM_KEYPTS), np.NaN)
+        people = frame['people']
+        if len(people) == 0:
+            return arr
+        for i in range(min(len(people), 3)): #3 people
+            for j in range(3): #x, y, and confidence
+                l = []
+                for keypt in people[i]:
+                    l.extend(people[i][keypt][j::3])
+                arr[i, j, :] = l
+        return arr
+    max_num_people = max(len(frame['people']) for frame in json_list)
+    flattened = np.array([frame_to_npy(frame_json, max_num_people)
+                          for frame_json in json_list])
+    return flattened
+    # json_filepaths = get_json_filepaths()
+    # json_list = load_json_list(json_filepaths)
+    # npy = json_list_to_npy(json_list)
+    # return npy
 def _openpose_filepath(tupl):
     """_openpose_filepath: applied function to generate filepaths for raw Openpose outputs.
 
@@ -109,10 +181,11 @@ def _openpose_filepath(tupl):
     filename = f'{vid_name}_{frame_num}_keypoints.json'
     return os.path.join(OPENPOSE_OUTPUT, vid_name, filename)
 
-def _save_vid_json(vid_df, overwrite): #Returns a list of Openpose JSONs for that video
+def _save_vid_json(vid_df, overwrite):
     """_save_vid_json: applied function to save a video df.
 
     :param vid_df: Pandas frame-level df for a given video.
+    :return: a list of Openpose JSONs for that video
     """
     vid_name = vid_df.name
     vid_df = vid_df.sort_values(by='frame')
@@ -171,7 +244,8 @@ def extract_face_hand(frame_df):
     return frame_df
 
 def extract_face_hand_video(frame_df):
-    """extract_face_hand_video: Extracts nose and wrist keypoints (corresponding to face and hand presence) for a given video's Openpose outputs.
+    """extract_face_hand_video: Extracts nose and wrist keypoints (corresponding to face
+    and hand presence) for a given video's Openpose outputs.
 
     :param frame_df: Dataframe with a row for each frame of video in the video.
     :return: list of [nose avg confidence, wrist avg confidence] for each frame of video
@@ -187,9 +261,10 @@ def extract_face_hand_frame(frame_keypts):
 
     :param frame_keypts: Dictionary of keypoints for an individual frame (Openpose format)
     :return: Average nose and average wrist confidence for the frame as a list
+    if working with data loaded in byte-format from msgpack files,
+    person['face_keypoints'] => person[b'face_keypoints'] and
+    frame_keypts['people'] => frame_keypts[b'people']
     """
-    # might need to convert all the below to person[b'face_keypoints'] and
-    # frame_keypts[b'people'], for example, if working with .msgpack files
 
     # Single nose keypoint confidence for each person.
     nose_keypts = [get_keypoint_conf(person['face_keypoints'], OPENPOSE_FACE_NOSE_KEYPT)
