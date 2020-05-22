de42b965ff1981fe95d243b7c8d32dbb174985f1
Ketan Agrawal <agrawalk@stanford.edu>
2020-05-21 23:24:45 -0400

    fixed interpolated f-strings

--- misc/download_databrary_volume.py
+++ misc/download_databrary_volume.py
@@ -26,25 +26,26 @@ def main():
 
 
 def login_databrary():
-    print('Logging into Databrary...')
     URL = "https://nyu.databrary.org//api/user/login"
     PARAMS = {'email': input('Databrary username? '),
               'password': getpass.getpass('Databrary password: ')}
+    print('Logging into Databrary...')
     s = requests.Session()
     r = s.post(url=URL, data=PARAMS)
     print(f'Result of login command (200 = success): {r.status_code}')
     return s
 
 def download_volume_info(s, volume_num, output_dir):
-    r = s.get('https://nyu.databrary.org/volume/{volume_num}/csv')
-    volume_info_csv = os.path.join(output_dir, 'volume_{volume_num}_info.csv')
+    print(f'Downloading volume {volume_num}\'s info csv...')
+    r = s.get(f'https://nyu.databrary.org/volume/{volume_num}/csv')
+    volume_info_csv = os.path.join(output_dir, f'volume_{volume_num}_info.csv')
     with open(volume_info_csv, 'wb') as f:
         f.write(r.content)
     return volume_info_csv
 
 def download_videos(s, volume_info_csv, output_dir, overwrite=False):
     def get_asset_ids_names(s, session_id):
-        r = s.get('https://nyu.databrary.org/api/slot/{session_id}/-?assets')
+        r = s.get(f'https://nyu.databrary.org/api/slot/{session_id}/-?assets')
         return [(asset['id'], asset['name']) for asset in r.json()['assets']]
 
     def url_for_download(session_id, asset_id):
@@ -54,15 +55,15 @@ def download_videos(s, volume_info_csv, output_dir, overwrite=False):
     vol_info = pd.read_csv(volume_info_csv)
     for i, row in vol_info.iterrows():
         session_id, session_name = row['session-id'], row['session-name']
-        print(f'Session {i+1}/len(vol_info): {session_name}')
+        print(f'Session {i+1}/{len(vol_info)}: {session_name}')
         for asset_id, asset_name in get_asset_ids_names(s, session_id):
             # Remove AVI from session names that accidentally have it
             vid_name = re.search(r'(.+)(?:\.AVI)?', asset_name)[1] + '.mp4'
             vid_path = os.path.join(output_dir, vid_name)
             if not overwrite and os.path.exists(vid_path):
-                print('{vid_name} already exists, continuing...')
+                print(f'{vid_name} already exists, continuing...')
                 continue
-            print('Downloading {vid_name}...')
+            print(f'Downloading {vid_name}...')
             r = s.get(url=url_for_download(session_id, asset_id))
             if (r.status_code != 200):
                 print(f'Got status code {r.status_code} downloading {vid_name}!')
