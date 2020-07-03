1aae629ead36bc9d3210ba243a7e01a92f615006
Ketan Agrawal <agrawalk@stanford.edu>
2020-07-03 00:16:10 -0400

    removed extraneous variables, updated defaults

--- openpose_detections/config.py
+++ openpose_detections/config.py
@@ -4,29 +4,24 @@ from default_config import *
 # TODO: Change these from defaults (commented out)!
 
 # List of directories containing videos in the dataset.
-# VIDEO_DIRS = ['/scratch/groups/mcfrank/Home_Headcam_new/']
+# VIDEO_DIRS = ['/scratch/groups/mcfrank/Home_Headcam_new/Alicecam',
+#               '/scratch/groups/mcfrank/Home_Headcam_new/Samcam',
+#               '/scratch/groups/mcfrank/Home_Headcam_new/Asacam']
 
 # Overall output directory.
-OUTPUT = os.path.join(os.path.expandvars('$GROUP_SCRATCH'), 'Home_Headcam_new', 'outputs')
+# OUTPUT = os.path.join(os.path.expandvars('$GROUP_SCRATCH'), 'Home_Headcam_new', 'outputs')
 
 # Directory for raw Openpose output (warning: need inode space for millions of small JSON files)
-OPENPOSE_OUTPUT = os.path.join(OUTPUT, 'openpose_raw_json')
+# OPENPOSE_OUTPUT = os.path.join(OUTPUT, 'openpose_raw_json')
 
 # Directory for condensed Openpose output (one file per video)
-OPENPOSE_CONDENSED_OUTPUT = os.path.join(OUTPUT, 'openpose_condensed')
-
-# File path for the video-level dataframe.
-# VID_DATAFRAME_PATH = os.path.join(OUTPUT, 'master_vid_info.json')
-
-# FIle path for the frame-level dataframe
-# (which will contain openpose outputs as well.)
-# FRAME_DATAFRAME_PATH = os.path.join(OUTPUT, 'master_frames_openpose.h5')
+# OPENPOSE_CONDENSED_OUTPUT = os.path.join(OUTPUT, 'openpose_condensed')
 
 # How much to rotate videos by (default 180, as most headcam videos are recorded upside down)
 # VID_ROTATE = 180
 
 # Dict from child id => birthdate in (year, month, day) format.
-# BIRTHDATES = {'A': (YYYY, MM, DD), 'S': (YYYY, MM, DD)}
+# BIRTHDATES = {'A': (YYYY, MM, DD), 'S': (YYYY, MM, DD), 'Y': (YYYY, MM, DD)}
 
 # Number of frames to extract for gold sample.
 # GOLD_SET_NUM_FRAMES = 24000
--- openpose_detections/default_config.py
+++ openpose_detections/default_config.py
@@ -4,9 +4,7 @@ import os
 VIDEO_DIRS = ['/scratch/groups/mcfrank/Home_Headcam_new/']
 
 #Overall output directory.
-OUTPUT = '/Users/brialong/Documents/GitHub/headcam-algo/data'
-
-
+OUTPUT = os.path.join(os.path.expandvars('$GROUP_SCRATCH'), 'Home_Headcam_new', 'outputs')
 # OUTPUT = '/scratch/users/agrawalk/headcam-algo-output/'
 
 # see https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md
@@ -33,27 +31,17 @@ NPY_HAND_LEFT_END = NPY_HAND_RIGHT_START = 109
 NPY_HAND_RIGHT_END = 130
 
 #Directory for raw Openpose output (warning: need inode space for millions of small JSON files)
-OPENPOSE_OUTPUT = os.path.join(OUTPUT, 'openpose')
+OPENPOSE_OUTPUT = os.path.join(OUTPUT, 'openpose_raw_json')
 
 #Directory for condensed Openpose output (one file per video) 
 OPENPOSE_CONDENSED_OUTPUT = os.path.join(OUTPUT, 'openpose_condensed')
-
-#File path for the video-level dataframe.
-VID_DATAFRAME_PATH = os.path.join(OUTPUT, 'master_vid_info.json')
-
-#File path for the frame-level dataframe
-#(which will contain openpose outputs as well.)
-FRAME_DATAFRAME_PATH = os.path.join(OUTPUT, 'master_frames_openpose.h5')
-
-#FIle path for the gold sample of frames used to evaluate detector performance.
-SAMPLE_DATAFRAME_PATH = os.path.join(OUTPUT, 'gold_sample.json')
 
 #How much to rotate videos by (default 180, as most headcam videos are recorded upside down)
 VID_ROTATE = 180
 
 #Dict from child id => birthdate in (year, month, day) format.
 #NOTE: Actual birthdates not included for privacy reasons.
-BIRTHDATES = {'A': (1980, 1, 1), 'S': (1980, 1, 1)}
+BIRTHDATES = {'A': (1980, 1, 1), 'S': (1980, 1, 1), 'Y': (1980, 1, 1)}
 
 #Number of frames to extract for gold sample.
 GOLD_SET_NUM_FRAMES = 24000
