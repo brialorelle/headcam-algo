8e70cdd034979c33d8552a020476176371f9bd28
Bria Long <brialorelle@gmail.com>
2022-01-04 15:49:03 -0800

    update demo script

--- openpose_detections/daylight_demo.py
+++ openpose_detections/daylight_demo.py
@@ -3,7 +3,7 @@ import os
 import ujson
 import multiprocessing as mp
 import time
-# import cv2
+import cv2
 import numpy as np
 # import matplotlib.pyplot as plt
 # from matplotlib import collections as mc
